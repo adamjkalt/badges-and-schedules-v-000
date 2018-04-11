069366cfabd40d7fc23924c955d7004a95556c7c
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-11 22:34:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |name|
-    return "Hello, my name is #{name}."
+  attendees.each do |badges|
+    return "Hello, my name is #{badges}."
   end
 end
 
